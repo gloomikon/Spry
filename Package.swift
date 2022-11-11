b1e3d333e3c11321ffbb2704190478a6bf4b7d75
КолумбиЯ <gloomikon@gmail.com>
2022-11-11 13:32:08 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Quick/Quick.git",
-                 .exact("3.1.2")),
+                 .upToNextMajor("6.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git",
-                 .exact("9.0.0"))
+                 .upToNextMajor("11.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
